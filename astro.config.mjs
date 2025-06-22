b72d9621a111f98e305d72f717ce31112e29ee10
KongLiu <kongliu.net@gmail.com>
2025-06-22 18:09:20 +0800

    Update header component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  site: 'https://kongliu.net',
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [icon()],
 });
