e79816f91ba99e2bc81fdabfb7ceb0b9a01521cd
KongLiu <kongliu.net@gmail.com>
2025-07-14 22:39:38 +0800

    Release version 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,30 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from 'astro-icon';
+
+import remarkDirective from "remark-directive"; /* handle ::: directives as nodes */
+import { remarkAdmonitions } from "./src/plugins/remark-admonitions"; /* add admonitions */
+import { remarkReadingTime } from "./src/plugins/remark-reading-time";
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: 'https://kongliu.net',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon(), mdx()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      themes: {
+        light: 'material-theme-lighter',
+        dark: 'material-theme-darker',
+      },
+    },
+    remarkPlugins: [remarkReadingTime, remarkDirective, remarkAdmonitions],
+  }
 });
