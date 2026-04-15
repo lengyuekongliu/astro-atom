c77d4e569946071fdeddcae48b72f074831479ca
KongLiu <admin@kongliu.net>
2026-04-15 18:43:14 +0800

    feat: update fonts and styles, refactor header and layout components
    - Removed Atkinson font files and replaced with Noto Serif SC and LXGW Wenkai Screen from CDN.
    - Refactored Header component to use MENU_LINKS for navigation and added icons for search and theme toggle.
    - Updated HeaderLink component to include underline effect on hover.
    - Introduced Layout component for consistent page structure across index and blog pages.
    - Simplified styles in Layout and global CSS, ensuring better responsiveness and theming.
    - Added new SVG icons for branding and navigation.
    - Updated consts.ts to reflect new site title and menu links in Chinese.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,24 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+import icon from 'astro-icon';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	}
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), icon()],
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                // 将 @ 映射到 src 目录
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+    }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,25 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'KongLiu\'s Blog';
+export const SITE_DESCRIPTION = 'Welcome to KongLiu\'s Blog!';
+
+
+export const MENU_LINKS: { path: string; title: string }[] = [
+	{
+		path: "/",
+		title: "主页",
+	},
+	{
+		path: "/blog",
+		title: "归档",
+	},
+	{
+		path: "/blog",
+		title: "标签",
+	},
+	{
+		path: "/about",
+		title: "关于",
+	},
+];
